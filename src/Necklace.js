bac52fc4168a5a4e86efc20ec7354367a23ada58
DanaHa-24 <dana24997@gmail.com>
2023-06-04 21:00:13 +0300

    improvements

--- src/Necklace.js
+++ src/Necklace.js
@@ -2,7 +2,7 @@ import Item from "./Item";
 
 export default class Necklace extends Item{
     constructor(id, name, price, color, type, image, style, status) {
-        super('2', id, name, price, color, type, image, status);
+        super('2', id, name, price, color, type, image, amount, status);
         
         //an array - like ['....','נופל','צוק'ר'] 
         this.style = style;
