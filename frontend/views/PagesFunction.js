ba937f6808fb1eb632f7bb5f9d8b677050c4c6ab
DanaHa-24 <dana24997@gmail.com>
2023-06-11 14:58:18 +0300

    finish Q&A page

--- frontend/views/PagesFunction.js
+++ frontend/views/PagesFunction.js
@@ -4,7 +4,7 @@ const accordionDataOrders = [
       content: ['בקלות! ניתן לבצע התחברות (בלחיצה על כפתור X למעלה) או לבצע רכישה ללא רישום, כאורח.', 'יש להזין את פרטי פרופיל המשתמש, מייל וסיסמא ולהתחיל להכניס את כל מה שאת/ה אוהב/ת לסל קניות.', 'בסיום יש לגשת לכפתור סיום הזמנה ולתשלום בקופה ובחירת אופן המשלוח.', 'במידה ויש לך קוד קופון יש להזינו בשדה "קוד קופון" - לפני שלב המעבר לתשלום.'] 
     },
     { title: 'איך אוכל לבדוק מה מצב ההזמנה שלי?', 
-      content: ['שאלה מצוינת! יש 2 דרכים לברר:', ' 1) .להיכנס למשתמש שלך ותחת קטגוריית ההזמנות שלי יופיע שדה סטטוס', '2) לפנות לצוות שירות הלקוחות שלנו במספר דרכי התקשרות : וואצאפ למספר 050-9008833 , שליחת הודעה באינסטגרם /   בפייסבוק שלנו.'] 
+      content: ['שאלה מצוינת! יש 2 דרכים לברר:', ' 1) להיכנס למשתמש שלך ותחת קטגוריית ההזמנות שלי יופיע שדה סטטוס.', '2) לפנות לצוות שירות הלקוחות שלנו במספר דרכי התקשרות : וואצאפ למספר 050-9008833 , שליחת הודעה באינסטגרם /   בפייסבוק שלנו.'] 
     },
     { title: 'איך אוכל לדעת שההזמנה בוצעה?', 
       content: ['בסיום ההזמנה יתקבל מסך של "הזמנתך בוצעה בהצלחה" עם מספר הזמנה בעל 10 ספרות לצורך מעקב.', 'כמו כן, מייל עם חשבונית יישלח לכתובת המייל אותה הזנת בעת מעמד הרכישה.'] 
@@ -30,20 +30,20 @@ const accordionDataOrders = [
     { title: 'הזמנתי חבילה לאיסוף מהסטודיו, מי יכול לאסוף אותה?',
       content: ['מי שביצע את ההזמנה או מישהו מטעמו בהצגת תעודה מזהה ומספר הזמנה.', '* חשוב לתאם הגעה מראש לסטודיו']
     }
-  ];
+];
 
 
-  const accordionDataShipments = [
+const accordionDataShipments = [
     { title: 'הזמנתי באתר, איך ההזמנה מגיעה אליי?',
-      content: ['אופציה ראשונה היא משלוח שמגיע עד 7 ימי עסקים, בעלות של 25 ₪.', 'אופציה נוספת היא להגיע ולאסוף מהסטודיו שלנו כ-3 ימי עסקים לאחר ההזמנה ללא תשלום נוסף. ', 'במהלך 3 ימי העסקים תישלח אלייך הודעה לתיאום ההגעה אלינו.', '* ערבי חג, ימי חג, שבת ו/או חגים אינם נחשבים ימי עסקים.']
+        content: ['אופציה ראשונה היא משלוח שמגיע עד 7 ימי עסקים, בעלות של 25 ₪.', 'אופציה נוספת היא להגיע ולאסוף מהסטודיו שלנו כ-3 ימי עסקים לאחר ההזמנה ללא תשלום נוסף. ', 'במהלך 3 ימי העסקים תישלח אלייך הודעה לתיאום ההגעה אלינו.', '* ערבי חג, ימי חג, שבת ו/או חגים אינם נחשבים ימי עסקים.']
     },
     { title: 'הזמנתי, עברו 7 ימי עסקים והמשלוח לא הגיע, מה עליי לעשות?',
-      content: ['שירות הלקוחות שלנו ישמח מאוד לעזור ולבדוק את הנושא, אפשר להתקשר או לשלוח הודעת וואצאפ למספר 050-9008833.', 'בנוסף, ניתן לפנות לעמוד שלנו בפייסבוק או באינסטגרם (חשוב לציין מספר הזמנה).', '* ערבי חג, ימי חג, שבת ו/או חגים אינם נחשבים ימי עסקים.']
+        content: ['שירות הלקוחות שלנו ישמח מאוד לעזור ולבדוק את הנושא, אפשר להתקשר או לשלוח הודעת וואצאפ למספר 050-9008833.', 'בנוסף, ניתן לפנות לעמוד שלנו בפייסבוק או באינסטגרם (חשוב לציין מספר הזמנה).', '* ערבי חג, ימי חג, שבת ו/או חגים אינם נחשבים ימי עסקים.']
     }
-  ];
+];
 
 
-  const accordionDataReturns = [
+const accordionDataReturns = [
     { title: 'איך מחליפים / מחזירים פריט שהזמנתי?',
       content: ['נשמח מאוד לפגוש אותך בסטודיו שלנו לצורך החלפה או החזרה - בתיאום הגעה מראש דרך הוואצאפ במספר 050-9008833.', 'אפשרות נוספת היא שליחת הפריט חזרה אלינו ע"י חברת שליחויות בעלות של 25 ₪.', 'לעוד פרטים תוכלי לפנות לצוות שירות לקוחות שלנו. ']
     },
@@ -53,11 +53,9 @@ const accordionDataOrders = [
     { title: 'כיצד מתבצע החזר כספי ותוך כמה זמן?',
       content: ['החזר כספי יינתן בתוך 14 ימי עסקים מרגע קבלת המשלוח,', 'ראשית יש להודיע במיידי על ההחזרה באחד מהערוצים הבאים - ווצאפ למספר 050-9008833 או דרך עמוד האינסטגרם / הפייסבוק שלנו. ', 'החזרת הפריט אלינו יכולה להתבצע ב-2 אופציות:', '1) הבאת הפריט לסטודיו שלנו בתוך 14 ימי עסקים מיום קבלתו ובתיאום מראש.', '2) החזרה דרך חברת שליחויות בעלות נוספת של 25 ₪ נוספים.', 'והכל בהתאם למועדים ולתנאים הקבועים בתקנות הגנת הצרכן (ביטול עסקה), התשע"א - 2010', 'במידה ובעת ביטול העסקה בוצע כבר המשלוח, לא יינתן החזר כספי בגין עלות השילוח. ', 'במקרה בו המשלוח ניתן בחינם במסגרת הטבה / מבצע, באם ביטול העסקה מבטל את התנאי למשלוח בהטבה, עלות המשלוח (להחזרת הפריט) תחול על הלקוח. ', 'בתום תהליך הזיכוי תשלח הודעה לכתובת המייל שהוזנה במעמד הרכישה.']
     }
-  ];
+];
 
-
-
-  const accordionDataPayments = [
+const accordionDataPayments = [
     { title: 'איך אפשר לשלם באתר?',
       content: ['באתר אפשר לשלם באשראי ע"י תשלום מאובטח ו/או בזיכוי ייעודי לאתר.']
     },
@@ -67,56 +65,81 @@ const accordionDataOrders = [
     { title: 'האם התשלום באתר מאובטח?',
       content: ['בהחלט! הרכישה באתר מאובטחת ועומדת בתקני הבטיחות המחמירים ביותר, פרטי האשראי שלך מוצפנים ובטוחים, ', 'אם עדיין יש חשש ניתן בשמחה להזמין גם טלפונים דרך צוות המוכרות שלנו במספר 050-9008833.']
     }
-  ];
+];
 
-
-  const accordionDataMaterials = [
+const accordionDataMaterials = [
     { title: 'האם יש אחריות על התכשיטים?',
       content: ['על כל תכשיט אנו מעניקים אחריות לשנה מיום רכישתו, בצירוף הקבלה. ', 'האחריות אינה כוללת שבר, קרע, מכות או שריטות, אחריות על הציפוי כתוצאה מחומציות או משימוש לא נכון בלאי ואובדן התכשיט או חלק ממנו.']
     },
     { title: 'מאילו חומרים עשויים התכשיטים ואיך לשמור על כל אחד מהם?',
-      content: ['כסף 925', 'הינה מתכת אצילית ואיכותית המחזיקה לאורך זמן, יחד עם זאת חומציות הגוף, החמצן שבאוויר,', 'מלחים וחומרים כימיים שונים עלולים לגרם להשחרה או הצהבה של הכסף,', 'אך אל דאגה עם שמירה וניקיון נכונים הכסף תמיד יחזור למצבו הטבעי. ', 'כדי לשמור על התכשיטים ניתן להשרות אותם אחת לתקופה במים חמימים עם נוזל כלים למשך 24 שעות,', 'ולאחר מכן לייבש היטב. ', '', 'גולדפילד', 'יש לשמור על התכשיט הרחק ממוצרי קוסמטיקה שונים, חומרי ניקוי ומגע בריכה וים שעלולים לפגוע בברק של התכשיט.', 'בנוסף ניתן לנקות אותם בסבון ומים אחת לתקופה במידת הצורך.', '', 'ציפוי זהב / ציפוי כסף', 'על מנת להחזיר לתכשיט מצופה את הברק יש לנקות אותו מלכלוך שומני ע"י ניקוי באמצעות מטלית רכה ויבשה בלבד.', 'מומלץ להימנע ממגע עם מים שכן גם הציפוי האיכותי ביותר ירד בשימוש לא נכון.', 'בשימוש לא נכון עם התכשיט לא תינתן אחריות על הציפוי.']
+      content: ['כסף 925', 'הינה מתכת אצילית ואיכותית המחזיקה לאורך זמן, יחד עם זאת חומציות הגוף, החמצן שבאוויר,', 'מלחים וחומרים כימיים שונים עלולים לגרם להשחרה או הצהבה של הכסף,', 'אך אל דאגה עם שמירה וניקיון נכונים הכסף תמיד יחזור למצבו הטבעי. ', 'כדי לשמור על התכשיטים ניתן להשרות אותם אחת לתקופה במים חמימים עם נוזל כלים למשך 24 שעות,', 'ולאחר מכן לייבש היטב. ', ' ', 'גולדפילד', 'יש לשמור על התכשיט הרחק ממוצרי קוסמטיקה שונים, חומרי ניקוי ומגע בריכה וים שעלולים לפגוע בברק של התכשיט.', 'בנוסף ניתן לנקות אותם בסבון ומים אחת לתקופה במידת הצורך.', ' ', 'ציפוי זהב / ציפוי כסף', 'על מנת להחזיר לתכשיט מצופה את הברק יש לנקות אותו מלכלוך שומני ע"י ניקוי באמצעות מטלית רכה ויבשה בלבד.', 'מומלץ להימנע ממגע עם מים שכן גם הציפוי האיכותי ביותר ירד בשימוש לא נכון.', 'בשימוש לא נכון עם התכשיט לא תינתן אחריות על הציפוי.']
     }
-  ];
+];
 
+const qaArraay = [accordionDataOrders, accordionDataShipments, accordionDataReturns, accordionDataPayments, accordionDataMaterials];
+const qaSections = ['הזמנות מהאתר', 'משלוחים', 'החזרות וזיכויים', 'תשלומים', 'חומרים ואחריות'];
 
+  
+function createAccordion(){
+    // 1) Creating a div for the entire content with the id "qa-accordions"
+    const accordionsContainer = $('<div>').attr('id', 'qa-accordions');
+    $('body').append(accordionsContainer);
 
-  
-  // Function to create accordion items
-  function createAccordionItems() {
-    const accordion = $('#accordion');
-  
-    accordionDataOrders.forEach((item, index) => {
-      const accordionItem = $('<div>').addClass('accordion-item');
-      const accordionHeader = $('<div>').addClass('accordion-header');
-      const accordionContent = $('<div>').addClass('accordion-content');
-  
-      const icon = $('<span>').addClass('icon').text('+');
-      const title = $('<span>').text(item.title);
-  
-      accordionHeader.append(icon, title);
-      accordionItem.append(accordionHeader, accordionContent);
-      accordion.append(accordionItem);
-  
-      // Build the content rows
-      const contentContainer = $('<div>').addClass('content-container');
-      item.content.forEach(row => {
-        const contentRow = $('<p>').addClass('content-row').text(row);
-        contentContainer.append(contentRow);
-      });
-      accordionContent.append(contentContainer);
-  
-      // Add event listener to toggle accordion item on click
-      accordionHeader.on('click', function() {
-        $(this).toggleClass('active');
-        $(this).siblings('.accordion-content').slideToggle();
-        $(this).find('.icon').text(function(_, text) {
-          return text === '+' ? '-' : '+';
+    // 2) Iterating over both arrays and performing the required actions
+    for (let i = 0; i < qaSections.length; i++) {
+        const section = qaSections[i];
+        const dataArray = qaArraay[i];
+
+        // 2.1) Creating the section title
+        const sectionTitle = $('<h6>').text(section);
+
+        // 2.2) Creating the accordion container
+        const accordionContainer = $('<div>').addClass('accordion-container');
+
+        // Iterating over the data array to create the accordion items
+        dataArray.forEach(item => {
+            // Creating accordion item elements
+            const accordionItem = $('<div>').addClass('accordion-item');
+            const accordionHeader = $('<div>').addClass('accordion-header');
+            const accordionContent = $('<div>').addClass('accordion-content');
+
+            // Creating the +/- button for opening/closing the accordion
+            const icon = $('<span>').addClass('icon').text('+');
+            const toggleButton = $('<button>').addClass('toggle-button').append(icon);
+
+            // Adding click event listener to toggle the accordion
+            toggleButton.on('click', function() {
+                $(this).toggleClass('active');
+                accordionContent.slideToggle();
+                icon.text(function(_, text) {
+                    return text === '+' ? '-' : '+';
+                });
+            });
+
+            // Setting the item text as the accordion title
+            const title = $('<span>').text(item.title);
+
+            // Adding the content rows to the accordion
+            const contentContainer = $('<div>').addClass('content-container');
+            item.content.forEach(row => {
+                const contentRow = $('<p>').addClass('content-row').text(row);
+                contentContainer.append(contentRow);
+            });
+
+            // Appending elements to create the accordion item
+            accordionHeader.append(toggleButton, title);
+            accordionContent.append(contentContainer);
+            accordionItem.append(accordionHeader, accordionContent);
+            accordionContainer.append(accordionItem);
         });
-      });
-    });
-  }
-  
-  // Call the function to create accordion items
-  createAccordionItems();
+
+        // Appending section title and accordion container to the main container
+        accordionsContainer.append(sectionTitle, accordionContainer);
+    }
+
+
+}  
+
+// Call the function to create accordion items
+createAccordion();
   
