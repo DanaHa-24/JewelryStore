e14042ebee6aa4e4730cc32af9e5d24641c11511
DanaHa-24 <dana24997@gmail.com>
2023-06-22 08:14:44 +0300

    reverse to earlier version

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
+
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
-// const branchRoutes = require('./routes/StoreBranchesRoute');
-// const itemRoutes = require('./routes/ItemRoute');
-// const configController = require('./controllers/ConfigController');
-// const addressRoutes = require('./routes/AddressRoute');
-// const cartRoutes = require ('./routes/CartRoute');
-// const orderRoutes = require('./routes/OrderRoute');
-// const userRoutes = require('./routes/UserRoute');
-// const wishlistRoutes = require('./routes/WishListRoute'); 
-// const itemController = require('./controllers/ItemController');
+const branchRoutes = require('./routes/StoreBranchesRoute');
+const itemRoutes = require('./routes/ItemRoute');
+const configController = require('./controllers/ConfigController');
+const addressRoutes = require('./routes/AddressRoute');
+const cartRoutes = require ('./routes/CartRoute');
+const configRoutes = require ('./routes/ConfigRoute');
+const orderRoutes = require('./routes/OrderRoute');
+const userRoutes = require('./routes/UserRoute');
+const wishlistRoutes = require('./routes/WishListRoute'); 
+const itemController = require('./controllers/ItemController');
 const cors = require('cors');
 console.log("hello");
 const uri = `mongodb+srv://admin:rachmany12345@cluster0.cpyytx0.mongodb.net/BU-db?retryWrites=true&w=majority`;
@@ -33,21 +34,19 @@ mongoose.connect(uri, options)
   app.use(cors());
   app.use(express.json());
 
-  app.use(bodyParser.urlencoded({extended:true}))
-
-  // app.use('/api/myaddresses', addressRoutes);
-  // app.use('/api/mycart', cartRoutes);
-  // app.use('/api/myorders', orderRoutes);
-  // app.use('/api/myuser', userRoutes);
-  // app.use('/api/mywishlist', wishlistRoutes);
-  // app.get('/api/config/api-key', configController.getApiKey);
-  // app.use('/api/config', require('./routes/ConfigRoute'));
-  // app.use('/api/storeBranches', branchRoutes);
-  // app.use('/Item', itemRoutes);
-  // app.get('/Item', itemController.getFullSchema);
+  app.use('/api/myaddresses', addressRoutes);
+  app.use('/api/mycart', cartRoutes);
+  app.use('/api/myorders', orderRoutes);
+  app.use('/api/myuser', userRoutes);
+  app.use('/api/mywishlist', wishlistRoutes);
+  app.get('/api/config/api-key', configController.getApiKey);
+  app.use('/api/config', configRoutes);
+  app.use('/api/storeBranches', branchRoutes);
+  app.use('/Item', itemRoutes);
+  app.get('/Item', itemController.getFullSchema);
 
 
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log('Backend server is running ');
 });
 
--- backend/routes/WishListRoute.js
+++ backend/routes/WishListRoute.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const WishlistController = require('../controllers/WishListController');
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
+
 router.get('/', WishlistController.getWishListItems);
 router.get('/test',WishlistController.createWishList);
 module.exports = router;
