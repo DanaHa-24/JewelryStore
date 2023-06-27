78249a9f89c100a576b6c1029720e020cbd7ded8
DanaHa-24 <dana24997@gmail.com>
2023-06-27 23:31:55 +0300

    Complete Item CRUD functionality except - need to seperate create and getallItems          to service and controller

--- backend/routes/ItemRoute.js
+++ backend/routes/ItemRoute.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router();
 const ItemController = require('../controllers/ItemController');
 
-router.get('/allItems', ItemController.getItems);
+// Get an item by ID
+router.get('/item/:id', ItemController.getItemById);
+
+// Search items by name
+router.get('/search', ItemController.searchItemsByName);
+
+// Get all items
+router.get('/allItems', ItemController.getAllItems);
+//router.get('/', ItemController.getAllItems);
+
+// Create a new item
+router.post('/', ItemController.createItem);
+
+// Update an item by ID
+router.put('/:id', ItemController.updateItem);
+
+// Delete an item by ID
+router.delete('/:id', ItemController.deleteItem);
 
 module.exports = router;
