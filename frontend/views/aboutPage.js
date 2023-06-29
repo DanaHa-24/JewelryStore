3fc16beb55f2b09e2daec20dfa88d132c4153649
DanaHa-24 <dana24997@gmail.com>
2023-06-29 18:04:14 +0300

    Align design

--- frontend/views/aboutPage.js
+++ frontend/views/aboutPage.js
@@ -1,35 +1,31 @@
+import { aboutUsText } from './constVars.js';
+
 $(document).ready(function() {
+    
     // Create container
-    const container = $('<div>').attr('id', 'container').addClass('container my-5 about-page-container');
+    const aboutContainer = $('<div>').attr('id', 'about-page-container').addClass('container my-5 about-page-container');
 
     // Create row
-    const row = $('<div>').addClass('row justify-content-center');
-    container.append(row);
+    const aboutRow = $('<div>').addClass('about-page-row d-flex justify-content-center align-items-center');
+    aboutContainer.append(aboutRow);
 
-    // Create column
-    const column = $('<div>').addClass('col-lg-8 text-center');
-    row.append(column);
+    // Create content container about us
+    const aboutContent = $('<div>').addClass('col-lg-8').attr('id', 'about-page-content');
+    aboutRow.append(aboutContent);
 
     // Create heading
-    const heading = $('<h2>').addClass('mb-4').text('הסיפור שלנו');
-    column.append(heading);
+    const aboutHeader = $('<h1>').addClass('mb-4').text('הסיפור שלנו').attr('id', 'about-page-header');
+    aboutContent.append(aboutHeader);  
 
-    // Create paragraphs
-    const paragraphs = [
-        'ברוכים הבאים ל-Be You, ', 'שם תכשיטים הם יותר מסתם קישוט - הם שיקוף של האומץ להיות עצמך.', 'בחרתי בשם "Be You" כי הוא מגלם את מהות המסע שלי בעולם. ',
-        'תמיד העזתי לאמץ את האינדיבידואליות שלי כשאחרים היססו, ואני רוצה שגם אתן.', 'Be You מסמל את הכוח של ביטוי עצמי, ומזכיר לנו שהיופי האמיתי טמון באותנטיות. ',
-        'עם כל פריט מעודן, אני מזמינה אותך לאמץ את הסיפור הייחודי שלך, ללבוש אותו בגאווה כדי שהעולם יראה.'
-    ];
-
-    paragraphs.forEach((text) => {
-        const paragraph = $('<p>').text(text);
-        column.append(paragraph);
+    aboutUsText.forEach((text) => {
+        const aboutLine = $('<p>').text(text);
+        aboutContent.append(aboutLine);
     });
 
     // Append image
-    const image = $('<img>').attr('src', "https://deih43ym53wif.cloudfront.net/kuta-bali-beach-indonesia-shutterstock_297303287.jpg_9b516347e5.jpg").addClass('img-fluid my-4');
-    column.append(image);
+    const aboutImage = $('<img>').attr('src', 'https://deih43ym53wif.cloudfront.net/kuta-bali-beach-indonesia-shutterstock_297303287.jpg_9b516347e5.jpg').addClass('img-fluid my-4');
+    aboutContent.append(aboutImage);
     
     // Append container to the body
-    $('body').append(container);
+    $('body').append(aboutContainer);
 });
