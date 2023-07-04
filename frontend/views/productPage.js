1d37225dfaee42ba8e03823d10942e03b2b7447b
EldarHalavia <eldar.halavia1@gmail.com>
2023-07-04 11:09:08 +0300

    reverse to origin files

--- frontend/views/productPage.js
+++ frontend/views/productPage.js
@@ -1,150 +1,44 @@
 $(document).ready(function() {
-    // Function to update product details dynamicly
-    function updateProductDetails(id) {
-      // Make a request to your MongoDB Atlas database to fetch the product details by ID
-      // Assuming you have set up the necessary connection and have the appropriate MongoDB client library installed
-      // Replace the connection and query logic with your actual implementation
-    
-      // Example using MongoDB Node.js driver
-      const { MongoClient } = require('mongodb');
-      const uri = 'mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority'; // Replace with your MongoDB Atlas connection string
-    
-      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    
-      client.connect((err) => {
-        if (err) {
-          console.error('Error connecting to MongoDB Atlas:', err);
-          return;
-        }
-    
-        const collection = client.db('your-database-name').collection('your-collection-name'); // Replace with your database and collection names
-    
-        collection.findOne({ id: id }, (err, product) => {
-          if (err) {
-            console.error('Error fetching product details from MongoDB Atlas:', err);
-            client.close();
-            return;
-          }
-    
-          // Update the product details on the page with the fetched data
-          $('.product-title').text(product.name);
-          $('.product-price').text(`מחיר: ₪${product.price}`).data('currency', 'shekels');
-          $('.product-description').text(product.description);
-    
-          // Check if it's the last couple of units
-          if (product.amountInStock <= 2) {
-            // Fetch additional information from the database based on the provided id
-            // and update the product availability accordingly
-            // You can customize this part based on your database structure and requirements
-            const additionalInfo = `יחידות אחרונות במלאי - צבעים זמינים: ${product.color.join(', ')}, גדלים זמינים: ${product.size.join(', ')}`;
-            $('.product-availability').text(additionalInfo);
-          } else {
-            $('.product-availability').text('מוצר במלאי');
-          }
-    
-          // Update the product image source dynamically
-          const productImage = $('<img>')
-            .attr('src', product.image)
-            .addClass('img-fluid rounded')
-            .attr('alt', 'Product Image');
-          $('.col-md-6.product-page-rtl').empty().append(productImage);
-    
-          client.close();
-        });
-      });
-    }
-    
-  
-    //live convertion rate webservice
-    function convertCurrency() {
-      const shekelsPrice = parseFloat($('.product-price').data('shekels'));
-      const currentCurrency = $('.product-price').data('currency');
-  
-      // Toggle between shekels and dollars
-      if (currentCurrency === 'shekels') {
-        const url = 'https://api.exchangerate-api.com/v4/latest/ILS';
-  
-        // Make a GET request to the web service
-        $.get(url, function(data) {
-          const exchangeRate = data.rates.USD;
-          const dollarsPrice = (shekelsPrice / exchangeRate).toFixed(2);
-  
-          $('.product-price').text(`מחיר: $${dollarsPrice}`).data('currency', 'dollars');
-        }).fail(function() {
-          // Display an error message if currency conversion fails
-          $('.product-price').text('מחיר: שגיאה').data('currency', 'shekels');
-        });
-      } else {
-        $('.product-price').text(`מחיר: ₪${shekelsPrice}`).data('currency', 'shekels');
-      }
-    }
-  
-    // Text content arrays
-    const productTitle = ['שם המוצר'];
-    const productPrice = ['מחיר: ₪0'];
-    const productAvailability = ['יחידות אחרונות במלאי'];
-    const productDescription = [
-      'תיאור המוצר מפורט כאן ומספק מידע על המאפיינים המרכזיים של המוצר. ניתן לכתוב על החומרים, העיצוב, הגימור וכל פרטים נוספים שיהיו רלוונטיים ללקוח.',
-    ];
-  
-    // Create the product container
-    const productContainer = $('<div>').addClass('container product-page-rtl');
-  
-    // Create the row element
-    const row = $('<div>').addClass('row');
-  
-    // Create the product image column
-    const imageColumn = $('<div>').addClass('col-md-6 product-page-rtl');
-    const productImage = $('<img>')
-      .attr('src', 'path/to/product-image.jpg')
-      .addClass('img-fluid rounded')
-      .attr('alt', 'Product Image');
-    imageColumn.append(productImage);
-  
-    // Create the product details column
-    const detailsColumn = $('<div>').addClass('col-md-6 text-right product-page-rtl');
-    detailsColumn.append($('<h1>').addClass('display-4 product-title product-page-rtl').text(productTitle[0]));
-  
-    // Create the price container
-    const priceContainer = $('<div>').addClass('d-flex align-items-center product-price-container product-page-rtl');
-    const priceText = $('<p>').addClass('lead mb-3 product-price product-page-rtl').text(productPrice[0]);
-    const priceButton = $('<button>').addClass('btn btn-secondary btn-sm ml-2 product-page-rtl product-page-convert').text('המר מטבע');
-    priceContainer.append(priceText);
-    priceContainer.append(priceButton);
-  
-    // Add click event to the price button
-    priceButton.on('click', convertCurrency);
-  
-    detailsColumn.append(priceContainer);
-    detailsColumn.append($('<p>').addClass('text-muted product-availability product-page-rtl').text(productAvailability[0]));
-    detailsColumn.append($('<hr>'));
-    const descriptionHeading = $('<h2>').addClass('h4 product-page-rtl').text('תיאור המוצר:');
-    descriptionHeading.css('margin-bottom', '10px');
-    detailsColumn.append(descriptionHeading);
-    detailsColumn.append($('<p>').addClass('text-muted product-description product-page-rtl').text(productDescription[0]));
-  
-    // Create a div to hold the buttons and apply margin between them
-    const buttonsDiv = $('<div>').addClass('product-page-rtl');
-    buttonsDiv.append($('<button>').addClass('btn btn-primary btn-lg product-page-rtl').text('הוסף לעגלת הקניות'));
-    buttonsDiv.append($('<button>').addClass('btn btn-secondary btn-lg ml-4 product-page-rtl').text('הוסף לרשימת המשאלות'));
-    buttonsDiv.children().css('margin-right', '10px');
-  
-    // Add the buttons div to the details column
-    detailsColumn.append(buttonsDiv);
-  
-  
-    // Append the columns and opinion window to the row
-    row.append(imageColumn);
-    row.append(detailsColumn);
-   
-  
-    // Append the row to the product container
-    productContainer.append(row);
-  
-    // Append the product container to the body
-    $('body').append(productContainer);
-  
-    // Update the product details with default content
-    updateProductDetails(productTitle[0], productPrice[0], productAvailability[0], productDescription[0]);
-  });
-  
+  // Text content arrays
+  const productTitle = ['שם המוצר'];
+  const productPrice = ['מחיר: ₪1,000'];
+  const productAvailability = ['יחידות אחרונות במלאי'];
+  const productDescription = ['תיאור המוצר מפורט כאן ומספק מידע על המאפיינים המרכזיים של המוצר. ניתן לכתוב על החומרים, העיצוב, הגימור וכל פרטים נוספים שיהיו רלוונטיים ללקוח.'];
+
+  // Create the product container
+  const productContainer = $('<div>').addClass('container');
+
+  // Create the row element
+  const row = $('<div>').addClass('row');
+
+  // Create the product image column
+  const imageColumn = $('<div>').addClass('col-md-6');
+  const productImage = $('<img>').attr('src', 'path/to/product-image.jpg').addClass('img-fluid rounded').attr('alt', 'Product Image');
+  imageColumn.append(productImage);
+
+  // Create the product details column
+  const detailsColumn = $('<div>').addClass('col-md-6 text-right');
+  detailsColumn.append($('<h1>').addClass('display-4').text(productTitle[0]));
+  detailsColumn.append($('<p>').addClass('lead mb-3').text(productPrice[0]));
+  detailsColumn.append($('<p>').addClass('text-muted').text(productAvailability[0]));
+  detailsColumn.append($('<hr>'));
+  const descriptionHeading = $('<h2>').addClass('h4').text('תיאור המוצר:');
+  descriptionHeading.css('margin-bottom', '10px');
+  detailsColumn.append(descriptionHeading);
+  detailsColumn.append($('<p>').addClass('text-muted').text(productDescription[0]));
+  detailsColumn.append($('<button>').addClass('btn btn-secondary btn-lg').text('הוסף לרשימת המשאלות'));
+  detailsColumn.append($('<button>').addClass('btn btn-primary btn-lg ml-4').text('הוסף לעגלת הקניות'));
+
+  // Add a common class to both buttons
+  detailsColumn.find('button').addClass('custom-button');
+
+  // Append the columns to the row
+  row.append(imageColumn);
+  row.append(detailsColumn);
+
+  // Append the row to the product container
+  productContainer.append(row);
+
+  // Append the product container to the body
+  $('body').append(productContainer);
+});
