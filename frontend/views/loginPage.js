be752d4fccb3e5de2cbd230c87baafeabfca3e19
barrachmany <rachmanybar@gmail.com>
2023-07-04 09:28:58 +0300

    fixing my part

--- frontend/views/loginPage.js
+++ frontend/views/loginPage.js
@@ -1,63 +1,88 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Create a canvas element
-  let canvas = $("<canvas>").attr("width", "200").attr("height", "1.5").attr("id","canvas-login-page");
+  let canvas = $('<canvas>').attr('width', '200').attr('height', '1.5').attr('id', 'canvas-login-page');
 
   // Create a login form
-  let loginForm = $("<div>").addClass("container").append(
-    $("<header>").text("התחברות").attr("id","login-page-header"),
-    $("<form>").addClass("row g-3").attr("id", "login-form").append(
-      $("<div>").addClass("col-12").append(
-        $("<input>").addClass("form-control").attr("type", "text").attr("placeholder", "אימייל").attr("id","login-page-input-email")
-      ),
-      $("<div>").addClass("col-12").append(
-        $("<input>").addClass("form-control").attr("type", "password").attr("placeholder", "סיסמה").attr("id","login-page-input-password")
-      ),
-      $("<div>").addClass("col-12").append(
-        $("<button>").addClass("btn btn-primary").text("התחבר/י").attr("id","login-page-button")
-      )
+  let loginForm = $('<div>')
+    .addClass('container')
+    .append(
+      $('<header>').text('התחברות').attr('id', 'login-page-header'),
+      $('<form>')
+        .addClass('row g-3')
+        .attr('id', 'login-form')
+        .append(
+          $('<div>')
+            .addClass('col-12')
+            .append(
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'text')
+                .attr('placeholder', 'אימייל')
+                .attr('id', 'login-page-input-email')
+            ),
+          $('<div>')
+            .addClass('col-12')
+            .append(
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'password')
+                .attr('placeholder', 'סיסמה')
+                .attr('id', 'login-page-input-password')
+            ),
+          $('<div>')
+            .addClass('col-12')
+            .append($('<button>').addClass('btn btn-primary').text('התחבר/י').attr('id', 'login-page-button'))
+        )
     )
-  ).attr("id","login-page-container");
+    .attr('id', 'login-page-container');
 
   // Append login form to the body
-  $("body").append(loginForm);
+  $('body').append(loginForm);
 
- // Append the canvas to the body
- $("body").append(canvas);
+  // Append the canvas to the body
+  $('body').append(canvas);
 
   // Create a button to redirect to the sign-up page
-  let signUpButton = $("<button>").addClass("btn btn-link").text("לא נרשמת עדיין? לחץ/י כאן להרשמה").attr("id","login-page-button-link").click(function() {
-    window.location.href = "signupPage.html"; // Replace "signup.html" with the URL of your sign-up page
-  });
+  let signUpButton = $('<button>')
+    .addClass('btn btn-link')
+    .text('לא נרשמת עדיין? לחץ/י כאן להרשמה')
+    .attr('id', 'login-page-button-link')
+    .click(function () {
+      window.location.href = 'signupPage.html'; // Replace "signup.html" with the URL of your sign-up page
+    });
 
   // Append the sign-up button to the body
-  $("body").append(signUpButton);
+  $('body').append(signUpButton);
 
-  $("#login-page-button").click(function(event){
+  $('#login-page-button').click(function (event) {
     event.preventDefault();
-    let emaiInputLogin = $("#login-page-input-email").val();
-    let passwordInputLogin = $("#login-page-input-password").val();
-    
+    let emaiInputLogin = $('#login-page-input-email').val();
+    let passwordInputLogin = $('#login-page-input-password').val();
+
     // Make an AJAX request to the server
-  $.ajax({
-      url: '/users/login',
+    $.ajax({
+      url: '/auth/login',
       method: 'POST',
       data: {
         username: emaiInputLogin,
-        password: passwordInputLogin
+        password: passwordInputLogin,
       },
-      success: function(response) {
+      success: function (response) {
         // Handle successful login
-        console.log(response);
+        const token = response.token;
+        const user = response.user; // The authenticated user
+
+        // Store the token in the browser's local storage
+        localStorage.setItem('token', token);
         // Redirect the user to the desired page after successful login
-        window.location.href = "homePage.html";
+        window.location.href = `/homePage.html`;
       },
-      error: function(error) {
+      error: function (error) {
         // Handle login error
         console.log(error);
         // Display an error message to the user
         alert('Invalid email or password. Please try again.');
-      }
+      },
     });
   });
-
 });
--- frontend/views/signupPage.js
+++ frontend/views/signupPage.js
@@ -1,175 +1,162 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Create a canvas element
-  let canvas = $("<canvas>")
-    .attr("width", "200")
-    .attr("height", "1.5")
-    .attr("id", "canvas-signin-page");
+  let canvas = $('<canvas>').attr('width', '200').attr('height', '1.5').attr('id', 'canvas-signin-page');
 
   // Create a signin form
-  let signinForm = $("<div>")
-    .addClass("container")
+  let signinForm = $('<div>')
+    .addClass('container')
     .append(
-      $("<header>").text("הרשמה").attr("id", "signin-page-header"),
-      $("<form>")
-        .addClass("row g-3")
-        .attr("id", "signin-form")
+      $('<header>').text('הרשמה').attr('id', 'signin-page-header'),
+      $('<form>')
+        .addClass('row g-3')
+        .attr('id', 'signin-form')
         .append(
-          $("<div>")
-            .addClass("col-12")
+          $('<div>')
+            .addClass('col-12')
             .append(
-              $("<input>")
-                .addClass("form-control")
-                .attr("type", "text")
-                .attr("placeholder", "שם פרטי")
-                .attr("id", "signin-page-input-firstName")
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'text')
+                .attr('placeholder', 'שם פרטי')
+                .attr('id', 'signin-page-input-firstName')
             ),
-          $("<div>")
-            .addClass("col-12")
+          $('<div>')
+            .addClass('col-12')
             .append(
-              $("<input>")
-                .addClass("form-control")
-                .attr("type", "text")
-                .attr("placeholder", "שם משפחה")
-                .attr("id", "signin-page-input-lastName")
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'text')
+                .attr('placeholder', 'שם משפחה')
+                .attr('id', 'signin-page-input-lastName')
             ),
-          $("<div>")
-            .addClass("col-12")
+          $('<div>')
+            .addClass('col-12')
             .append(
-              $("<input>")
-                .addClass("form-control")
-                .attr("type", "text")
-                .attr("placeholder", "אימייל")
-                .attr("id", "signin-page-input-email")
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'text')
+                .attr('placeholder', 'אימייל')
+                .attr('id', 'signin-page-input-email')
             ),
-          $("<div>")
-            .addClass("col-12")
+          $('<div>')
+            .addClass('col-12')
             .append(
-              $("<input>")
-                .addClass("form-control")
-                .attr("type", "text")
-                .attr(
-                  "placeholder",
-                  "כתובת - עיר, רחוב, מספר בית, מספר דירה/כניסה, מיקוד"
-                )
-                .attr("id", "signin-page-input-address")
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'text')
+                .attr('placeholder', 'כתובת - עיר, רחוב, מספר בית, מספר דירה/כניסה, מיקוד')
+                .attr('id', 'signin-page-input-address')
             ),
-          $("<div>")
-            .addClass("col-12")
+          $('<div>')
+            .addClass('col-12')
             .append(
-              $("<input>")
-                .addClass("form-control")
-                .attr("type", "text")
-                .attr("placeholder", "מספר טלפון")
-                .attr("id", "signin-page-input-phoneNumber")
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'text')
+                .attr('placeholder', 'מספר טלפון')
+                .attr('id', 'signin-page-input-phoneNumber')
             ),
-          $("<div>")
-            .addClass("col-12")
+          $('<div>')
+            .addClass('col-12')
             .append(
-              $("<input>")
-                .addClass("form-control")
-                .attr("type", "password")
-                .attr("placeholder", "סיסמה")
-                .attr("id", "signin-page-input-password")
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'password')
+                .attr('placeholder', 'סיסמה')
+                .attr('id', 'signin-page-input-password')
             ),
-          $("<div>")
-            .addClass("col-12")
+          $('<div>')
+            .addClass('col-12')
             .append(
-              $("<input>")
-                .addClass("form-control")
-                .attr("type", "password")
-                .attr("placeholder", "הזן סיסמה שנית")
-                .attr("id", "signin-page-input-confirmPassword")
+              $('<input>')
+                .addClass('form-control')
+                .attr('type', 'password')
+                .attr('placeholder', 'הזן סיסמה שנית')
+                .attr('id', 'signin-page-input-confirmPassword')
             ),
-          $("<div>")
-            .addClass("col-12")
-            .append(
-              $("<button>")
-                .addClass("btn btn-primary")
-                .text("הרשם/י")
-                .attr("id", "signin-page-button")
-            )
+          $('<div>')
+            .addClass('col-12')
+            .append($('<button>').addClass('btn btn-primary').text('הרשם/י').attr('id', 'signin-page-button'))
         )
     )
-    .attr("id", "signin-page-container");
+    .attr('id', 'signin-page-container');
 
   // Append login form to the body
-  $("body").append(signinForm);
+  $('body').append(signinForm);
 
   // Append the canvas to the body
-  $("body").append(canvas);
+  $('body').append(canvas);
 
   // Create a button to redirect to the sign-up page
-  let signUpButton = $("<button>")
-    .addClass("btn btn-link")
-    .text("נרשמת כבר בעבר? לחץ/י כאן להתחברות")
-    .click(function() {
-      window.location.href = "loginPage.html"; // Replace "signup.html" with the URL of your sign-up page
+  let signUpButton = $('<button>')
+    .addClass('btn btn-link')
+    .text('נרשמת כבר בעבר? לחץ/י כאן להתחברות')
+    .click(function () {
+      window.location.href = 'loginPage.html'; // Replace "signup.html" with the URL of your sign-up page
     })
-    .attr("id", "signin-page-button-link");
+    .attr('id', 'signin-page-button-link');
 
   // Append the sign-up button to the body
-  $("body").append(signUpButton);
+  $('body').append(signUpButton);
 
-  
-  $("#signin-page-button").click(function(event) {
+  $('#signin-page-button').click(function (event) {
     event.preventDefault();
-    let firstNameSignin = $("#signin-page-input-firstName").val();
-    let lastNameSignin = $("#signin-page-input-lastName").val();
-    let emailSignin = $("#signin-page-input-email").val(); // Assuming email field is used as username
-    let addressSignin = $("#signin-page-input-address").val().split(",");
-    let phoneSignin = $("#signin-page-input-phoneNumber").val();
-    let passwordSignin = $("#signin-page-input-password").val();
-    let confirmPasswordSignin = $("#signin-page-input-confirmPassword").val();
-  
+    let firstNameSignin = $('#signin-page-input-firstName').val();
+    let lastNameSignin = $('#signin-page-input-lastName').val();
+    let emailSignin = $('#signin-page-input-email').val();
+    let addressSignin = $('#signin-page-input-address').val().split(',');
+    let phoneSignin = $('#signin-page-input-phoneNumber').val();
+    let passwordSignin = $('#signin-page-input-password').val();
+    let confirmPasswordSignin = $('#signin-page-input-confirmPassword').val();
+    console.log(addressSignin);
     // Create an object with the address data
     const addressData = {
-      city: addressSignin[0].trim(),
-      street: addressSignin[1].trim(),
-      houseNum: parseInt(addressSignin[2].trim()),
-      apartmentNum: parseInt(addressSignin[3].trim()),
-      postalCode: parseInt(addressSignin[4].trim())
+      city: addressSignin[0],
+      street: addressSignin[1],
+      houseNum: parseInt(addressSignin[2]),
+      apartmentNum: parseInt(addressSignin[3]),
+      postalCode: parseInt(addressSignin[4]),
     };
-  
+
     // Send a POST request to create the address
     $.ajax({
-      type: "POST",
-      url: "/addresses",
+      url: '/addresses',
+      type: 'POST',
       data: addressData,
-      success: function(addressResponse) {
+      success: function (addressResponse) {
         // Create an object with the form data including the address ID
         const formData = {
           firstName: firstNameSignin,
           lastName: lastNameSignin,
-          username: emailSignin, // Assuming email field is used as username
+          username: emailSignin,
           password: passwordSignin,
           address: [addressResponse._id],
-          phoneNumber: phoneSignin
+          phoneNumber: phoneSignin,
         };
-  
+
         // Send a POST request to create the user with the updated address
         $.ajax({
-          type: "POST",
-          url: "/users",
+          type: 'POST',
+          url: '/auth/register',
           data: formData,
-          success: function(userResponse) {
+          success: function (userResponse) {
             // Handle the success response
-            console.log("you logged in");
+            console.log(userResponse);
             // Redirect to a success page or perform any other actions
-            window.location.href = `/homePage.html`;
+            window.location.href = `/loginPage.html`;
           },
-          error: function(userError) {
+          error: function (userError) {
             // Handle the error response for user creation
             console.log(userError);
             // Display an error message or perform any other actions
-          }
+          },
         });
       },
-      error: function(addressError) {
+      error: function (addressError) {
         // Handle the error response for address creation
         console.log(addressError);
         // Display an error message or perform any other actions
-      }
+      },
     });
-  }); 
-  
+  });
 });
