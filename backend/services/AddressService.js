fb21fab3a60fc3ca93db653783df105229254861
EldarHalavia <eldar.halavia1@gmail.com>
2023-07-10 21:31:01 +0300

    adding new services according to new controller's methods

--- backend/services/AddressService.js
+++ backend/services/AddressService.js
@@ -1,4 +1,5 @@
 const Address = require('../models/AddressSchema');
+const User = require('../models/UserSchema');
 
 // Get all addresses
 async function getAllAddresses() {
@@ -11,48 +12,52 @@ async function getAllAddresses() {
   }
 }
 
-
 // Create a new address
-async function createAddress(addressData) {
-    try {
-      const newAddress = await Address.create(addressData);
-      return newAddress;
-    } catch (error) {
-      console.error('Error creating address:', error);
-      throw new Error('Failed to create address');
-    }
+async function createAddress(userId, addressData) {
+  try {
+    const user = await User.findById(userId);
+    if (!user) throw new Error('User not found');
+    const newAddress = await Address.create(addressData);
+    newAddress.user = user;
+    await newAddress.save();
+
+    user.address.push(newAddress._id);
+    await user.save();
+
+    return newAddress;
+  } catch (error) {
+    console.error('Error creating address:', error);
+    throw new Error('Failed to create address');
+  }
 }
-  
 
 // Delete an address by ID
 async function deleteAddress(addressId) {
-    try {
-      const deletedAddress = await Address.findByIdAndDelete(addressId);
-      if (!deletedAddress) {
-        throw new Error('Address not found');
-      }
-      return deletedAddress;
-    } catch (error) {
-      console.error('Error deleting address:', error);
-      throw new Error('Failed to delete address');
+  try {
+    const deletedAddress = await Address.findByIdAndDelete(addressId);
+    if (!deletedAddress) {
+      throw new Error('Address not found');
     }
+    return deletedAddress;
+  } catch (error) {
+    console.error('Error deleting address:', error);
+    throw new Error('Failed to delete address');
+  }
 }
-  
 
 // Update an address by ID
 async function updateAddress(addressId, updatedData) {
-    try {
-      const updatedAddress = await Address.findByIdAndUpdate(addressId, updatedData, { new: true });
-      if (!updatedAddress) {
-        throw new Error('Address not found');
-      }
-      return updatedAddress;
-    } catch (error) {
-      console.error('Error updating address:', error);
-      throw new Error('Failed to update address');
+  try {
+    const updatedAddress = await Address.findByIdAndUpdate(addressId, updatedData, { new: true });
+    if (!updatedAddress) {
+      throw new Error('Address not found');
     }
+    return updatedAddress;
+  } catch (error) {
+    console.error('Error updating address:', error);
+    throw new Error('Failed to update address');
+  }
 }
-
 
 // Get an address by ID
 async function getAddressById(addressId) {
@@ -67,46 +72,40 @@ async function getAddressById(addressId) {
   }
 }
 
-
 // Search addresses by name, city, or street
 async function searchAddresses(query) {
-    try {
-      const addresses = await Address.find({
-        $or: [
-          { name: { $regex: query, $options: 'i' } },
-          { city: { $regex: query, $options: 'i' } },
-          { street: { $regex: query, $options: 'i' } },
-        ],
-      });
-      return addresses;
-    } catch (error) {
-      console.error('Error searching addresses:', error);
-      throw new Error('Failed to search addresses');
-    }
+  try {
+    const addresses = await Address.find({
+      $or: [
+        { name: { $regex: query, $options: 'i' } },
+        { city: { $regex: query, $options: 'i' } },
+        { street: { $regex: query, $options: 'i' } },
+      ],
+    });
+    return addresses;
+  } catch (error) {
+    console.error('Error searching addresses:', error);
+    throw new Error('Failed to search addresses');
+  }
 }
-  
 
 // Get addresses for a specific user
 async function getUserAddresses(userId) {
-    try {
-      const addresses = await Address.find({ user: userId }); // Assuming the user field in the Address model represents the user associated with the address
-      return addresses;
-    } catch (error) {
-      console.error('Error getting user addresses:', error);
-      throw new Error('Failed to get user addresses');
-    }
+  try {
+    const addresses = await Address.find({ user: userId }); // Assuming the user field in the Address model represents the user associated with the address
+    return addresses;
+  } catch (error) {
+    console.error('Error getting user addresses:', error);
+    throw new Error('Failed to get user addresses');
+  }
 }
-  
 
 module.exports = {
-    createAddress,
-    deleteAddress,
-    updateAddress,
-    getAllAddresses,
-    getAddressById,
-    searchAddresses,
-    getUserAddresses
+  createAddress,
+  deleteAddress,
+  updateAddress,
+  getAllAddresses,
+  getAddressById,
+  searchAddresses,
+  getUserAddresses,
 };
-
-
-
--- backend/services/WishListService.js
+++ backend/services/WishListService.js
@@ -2,140 +2,139 @@ const Wishlist = require('../models/WishListSchema');
 
 // Get all items in the wishlist for the authenticated user (by ID)
 async function getAllWishlistItems(wishlistId) {
-    try {
+  try {
     const wishList = await Wishlist.findById(wishlistId);
 
     if (!wishList) {
-        throw new Error('Wishlist not found');
+      throw new Error('Wishlist not found');
     }
 
     return wishList.items;
-    } catch (error) {
+  } catch (error) {
     console.error('Error getting wishlist items:', error);
     throw new Error('Failed to get wishlist items');
-    }
+  }
 }
-
 
 // Create a new wishlist the authenticated user (by ID)
 async function createWishlist(userId) {
-    try {
-        const wishList = new Wishlist({ user: userId });
-        await wishList.save();
-        return wishList;
-    } catch (error) {
-        console.error('Error creating wishlist:', error);
-        throw new Error('Failed to create wishlist');
-    }
+  try {
+    const wishList = new Wishlist({ user: userId });
+    await wishList.save();
+    return wishList;
+  } catch (error) {
+    console.error('Error creating wishlist:', error);
+    throw new Error('Failed to create wishlist');
+  }
 }
-  
 
 // Delete the wishlist for the authenticated user (by ID)
 async function deleteWishlist(userId) {
-    try {
+  try {
     await Wishlist.deleteOne({ user: userId });
-    } catch (error) {
+  } catch (error) {
     console.error('Error deleting wishlist:', error);
     throw new Error('Failed to delete wishlist');
-    }
+  }
 }
-
 
 // Remove an item from the wishlist for the authenticated user (by ID)
-async function removeItem(wishlistId, itemId) {
-    try {
-      await Wishlist.findByIdAndUpdate(wishlistId, { $pull: { items: itemId } });
-    } catch (error) {
-      console.error('Error deleting item from wishlist:', error);
-      throw new Error('Failed to delete item from wishlist');
-    }
+async function removeItem(userId, itemId) {
+  try {
+    const wishList = await Wishlist.findOne({ user: userId });
+    wishList.items.pull(itemId);
+    await wishList.save();
+    return wishList;
+  } catch (error) {
+    console.error('Error deleting item from wishlist:', error);
+    throw new Error('Failed to delete item from wishlist');
+  }
 }
-  
 
 // Update the wishlist
 async function updateWishlist(wishlistId, wishlistData) {
-    try {
-      const updatedWishlist = await Wishlist.findByIdAndUpdate(
-        wishlistId,
-        wishlistData,
-        { new: true }
-      );
-      return updatedWishlist;
-    } catch (error) {
-      console.error('Error updating wishlist:', error);
-      throw new Error('Failed to update wishlist');
-    }
+  try {
+    const updatedWishlist = await Wishlist.findByIdAndUpdate(wishlistId, wishlistData, { new: true });
+    return updatedWishlist;
+  } catch (error) {
+    console.error('Error updating wishlist:', error);
+    throw new Error('Failed to update wishlist');
+  }
 }
-  
 
 // Get a specific wishlist by ID
 async function getWishlistById(wishlistId) {
-    try {
-      const wishlist = await Wishlist.findById(wishlistId);
-      return wishlist;
-    } catch (error) {
-      console.error('Error retrieving wishlist:', error);
-      throw new Error('Failed to retrieve wishlist');
-    }
+  try {
+    const wishlist = await Wishlist.findById(wishlistId);
+    return wishlist;
+  } catch (error) {
+    console.error('Error retrieving wishlist:', error);
+    throw new Error('Failed to retrieve wishlist');
+  }
 }
-  
- 
+
 // Search for items in the wishlist for the authenticated user (by ID)
 async function searchWishlistItems(wishlistId, itemName) {
-    try {
-      const wishlist = await Wishlist.findById(wishlistId).populate({
-        path: 'items',
-        match: { name: { $regex: itemName, $options: 'i' } }
-      });
-  
-      if (!wishlist) {
-        throw new Error('Wishlist not found');
-      }
-  
-      const items = wishlist.items.filter((item) =>
-        item.name.toLowerCase().includes(itemName.toLowerCase())
-      );
-  
-      return items;
-    } catch (error) {
-      console.error('Error searching wishlist items:', error);
-      throw new Error('Failed to search wishlist items');
+  try {
+    const wishlist = await Wishlist.findById(wishlistId).populate({
+      path: 'items',
+      match: { name: { $regex: itemName, $options: 'i' } },
+    });
+
+    if (!wishlist) {
+      throw new Error('Wishlist not found');
     }
+
+    const items = wishlist.items.filter((item) => item.name.toLowerCase().includes(itemName.toLowerCase()));
+
+    return items;
+  } catch (error) {
+    console.error('Error searching wishlist items:', error);
+    throw new Error('Failed to search wishlist items');
+  }
 }
-  
 
 // Add an item to the wishlist for the authenticated user (by ID)
-async function addItem(wishlistId, itemId) {
-    try {
-      const wishList = await Wishlist.findById(wishlistId);
-  
-      if (!wishList) {
-        throw new Error('Wishlist not found');
-      }
-  
-      wishList.items.push(itemId);
-      await wishList.save();
-  
-      return wishList;
-    } catch (error) {
-      console.error('Error adding item to wishlist:', error);
-      throw new Error('Failed to add item to wishlist');
+async function addItem(userId, itemId) {
+  try {
+    const wishList = await Wishlist.findOne({ user: userId });
+
+    if (!wishList) throw new Error('Wishlist not found');
+
+    // Check if item already exists in wishlist
+    if (wishList.items.includes(itemId)) {
+      throw new Error('Item already exists in wishlist');
     }
+
+    wishList.items.push(itemId);
+    await wishList.save();
+
+    return wishList;
+  } catch (error) {
+    console.error('Error adding item to wishlist:', error);
+    throw new Error('Failed to add item to wishlist');
+  }
 }
-  
+
+// Get user wishlist
+async function getUserWishlist(userId) {
+  try {
+    const wishList = await Wishlist.findOne({ user: userId }).populate('items');
+    return wishList;
+  } catch (error) {
+    console.error('Error getting wishlist:', error);
+    throw new Error('Failed to get wishlist');
+  }
+}
 
 module.exports = {
-    getAllWishlistItems,
-    createWishlist,
-    deleteWishlist,
-    removeItem,
-    updateWishlist,
-    getWishlistById,
-    searchWishlistItems,
-    addItem
+  getAllWishlistItems,
+  createWishlist,
+  deleteWishlist,
+  removeItem,
+  updateWishlist,
+  getWishlistById,
+  searchWishlistItems,
+  addItem,
+  getUserWishlist,
 };
-
-
-
-
-                
